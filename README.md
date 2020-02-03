6bc8fae4c356c5c075d687fd80be6e9407302b30
unknown <ybalimova@gmail.com>
2020-02-03 13:58:43 +0300

    fix appveyor

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/n8qeh8tw9cdlueia?svg=true)](https://ci.appveyor.com/project/juliapolyakova/ahj1-env)
+https://juliapolyakova.github.io/ahj1_env/
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,6 +11,13 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
@@ -43,5 +51,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
+    new CopyWebpackPlugin([
+      { from: 'src/img', to: 'img' },
+    ]),
   ],
 };
